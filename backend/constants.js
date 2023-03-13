54d6e160c38163116be9814bf03c9d9f2e6e6c33
blachua <116645635+blachua@users.noreply.github.com>
2023-03-13 17:04:57 +0800

    Initial Commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xA4EEDA8363ae7bDf22065178b43D8f67deD2B26F";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
